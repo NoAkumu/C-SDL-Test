bae5e080e83c2b7286b83b6a97b6e059e1d3fb37
NoAkumu <nakumu90@gmail.com>
2026-03-01 05:32:05 -0300

    Added .gitignore

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cc -o ./game ./src/game.c `sdl2-config --cflags --libs` -lm -lSDL2_image 
-./game -ex r 
+cc -o ./game.out ./src/game.c `sdl2-config --cflags --libs` -lm -lSDL2_image 
+./game.out -ex r 
